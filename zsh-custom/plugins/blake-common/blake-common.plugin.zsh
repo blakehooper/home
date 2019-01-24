7624c8a67c476a5d5f280fc1e63ac78a9e8c89bd
Blake Hooper <blake.hooper@fairfaxmedia.co.nz>
2019-01-25 08:43:24 +1300

    Add remote search to chbr function

--- zsh-custom/plugins/blake-common/blake-common.plugin.zsh
+++ zsh-custom/plugins/blake-common/blake-common.plugin.zsh
@@ -3,10 +3,26 @@ alias find='noglob find'
 alias ws='cd ~/workspace'
 
 chbr() {
-  possibleBranches=("${(@f)$(git branch | cut -c 3- | grep $1 )}")
+  # possibleBranches=("${(@f)$(git branch | cut -c 3- | grep $1 )}")
+  IFS=$'\n'
+  # possibleBranches=("${(@f)$(git branch | cut -c 3- | grep $1 )}")
+  possibleBranches=($(git branch | cut -c 3- | grep $1 | sed 's/:.*//'))
+  unset IFS
   if [ ${#possibleBranches} -ge 2 ]; then
     echo 'More than 1 match'
     for x in $possibleBranches; do echo $x; done
+  elif [ ${#possibleBranches} -eq 0 ]; then
+    echo '0 branches locally. Searching remote...'
+    IFS=$'\n'
+    possibleBranches=($(git branch -r | cut -c 3- | grep $1 | sed 's/:.*//'))
+    unset IFS
+    if [ ${#possibleBranches} -ge 2 ]; then
+      echo 'More than 1 match'
+      for x in $possibleBranches; do echo $x; done
+    else
+      echo $possibleBranches[1]
+      git co ${possibleBranches[1]#*/}
+    fi
   else
     # not 0 index for some reason...
     git co $possibleBranches[1]
